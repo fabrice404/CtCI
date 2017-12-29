b4f2ce1c3c94e7c134b5988c9cd684ca2aa68b1d
fabrice404 <fabrice.lamant@gmail.com>
2017-12-29 11:10:44 +0100

    Chapter 2 - Questions 1 & 2

---
+++ Chapters/Chapter02.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CtCI
+{
+    public class Chapter02
+    {
+        // 2.1 Remove Dups
+        public void Question01(LinkedListNode head)
+        {
+            var values = new List<int>();
+            LinkedListNode current = head;
+
+            while (current.next != null)
+            {
+                values.Add(current.data);
+                if (values.Contains(current.next.data))
+                {
+                    current.next = current.next.next;
+                }
+                current = current.next;
+            }
+        }
+
+        // 2.2 Return Kth to Last 
+        public LinkedListNode Question02(LinkedListNode head, int k)
+        {
+            LinkedListNode current = head,
+                runner = head;
+            for (int i = 0; i < k; i++)
+            {
+                runner = runner.next;
+            }
+
+            while (runner.next != null)
+            {
+                runner = runner.next;
+                current = current.next;
+            }
+
+            return current;
+        }
+    }
+}
---
+++ Tests/Chapter02Test.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CtCI
+{
+    [TestFixture]
+    public class Chapter02Test
+    {
+        [TestCase]
+        public void Question01()
+        {
+            Chapter02 chapter02 = new Chapter02();
+            LinkedListNode root = new LinkedListNode(1);
+            root.AddNode(2).AddNode(3).AddNode(3).AddNode(4).AddNode(5);
+
+            Assert.AreEqual(1, root.data);
+            Assert.AreEqual(2, root.next.data);
+            Assert.AreEqual(3, root.next.next.data);
+            Assert.AreEqual(3, root.next.next.next.data);
+            Assert.AreEqual(4, root.next.next.next.next.data);
+            Assert.AreEqual(5, root.next.next.next.next.next.data);
+
+            chapter02.Question01(root);
+
+            Assert.AreEqual(1, root.data);
+            Assert.AreEqual(2, root.next.data);
+            Assert.AreEqual(3, root.next.next.data);
+            Assert.AreEqual(4, root.next.next.next.data);
+            Assert.AreEqual(5, root.next.next.next.next.data);
+        }
+
+        [TestCase]
+        public void Question02()
+        {
+            Chapter02 chapter02 = new Chapter02();
+            LinkedListNode root = new LinkedListNode(1);
+            root.AddNode(2).AddNode(3).AddNode(4).AddNode(5)
+                .AddNode(6).AddNode(7).AddNode(8).AddNode(9);
+
+            Assert.AreEqual(8, chapter02.Question02(root, 1).data);
+            Assert.AreEqual(2, chapter02.Question02(root, 7).data);
+        }
+    }
+}

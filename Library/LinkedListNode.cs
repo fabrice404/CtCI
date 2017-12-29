8593e2e38e45800c79dfcd25e78f2d4a1b6746af
fabrice404 <fabrice.lamant@gmail.com>
2017-12-29 11:10:24 +0100

    Add LinkedListNode class

---
+++ Library/LinkedListNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CtCI
+{
+    public class LinkedListNode
+    {
+        public LinkedListNode next = null;
+        public int data;
+
+        public LinkedListNode(int data)
+        {
+            this.data = data;
+        }
+
+        public LinkedListNode AddNode(int data)
+        {
+            LinkedListNode last = new LinkedListNode(data);
+            LinkedListNode node = this;
+            while(node.next != null) {
+                node = node.next;
+            }
+            node.next = last;
+            return node.next;
+        }
+    }
+}

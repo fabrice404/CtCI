a667355995fe317768b9a3fe7299aa370be8820a
fabrice404 <fabrice.lamant@gmail.com>
2017-12-30 16:36:03 +0100

    Chapter 3 - Question 2

---
+++ Chapters/Chapter03/Question02.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace CtCI.Chapter3
+{
+    public class Q2Stack
+    {
+        private Q2StackNode top;
+
+        public int Pop()
+        {
+            if (this.top == null)
+            {
+                throw new Exception();
+            }
+            var result = this.top.data;
+            this.top = this.top.next;
+            return result;
+        }
+
+        public int Peek()
+        {
+            if (this.top == null)
+            {
+                throw new Exception();
+            }
+            return this.top.data;
+        }
+
+        public void Push(int data)
+        {
+            var t = new Q2StackNode(data)
+            {
+                next = this.top,
+                min = (this.top == null || data < this.top.data) ? data : this.top.data
+            };
+            this.top = t;
+        }
+
+        public int Min()
+        {
+            if (top == null)
+            {
+                throw new Exception();
+            }
+            return top.min;
+        }
+    }
+
+    public class Q2StackNode
+    {
+        public int data;
+        public int min;
+        public Q2StackNode next;
+
+        public Q2StackNode(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ Tests/Chapter03Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CtCI
+{
+    [TestFixture]
+    public class Chapter03Test
+    {
+        [TestCase]
+        public void Question01() { }
+
+        [TestCase]
+        public void Question02()
+        {
+            var stack = new Chapter3.Q2Stack();
+
+            stack.Push(5);
+            Assert.AreEqual(5, stack.Min());
+
+            stack.Push(6);
+            Assert.AreEqual(5, stack.Min());
+
+            stack.Push(3);
+            Assert.AreEqual(3, stack.Min());
+
+            stack.Push(7);
+            Assert.AreEqual(3, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(3, stack.Min());
+
+            stack.Pop();
+            Assert.AreEqual(5, stack.Min());
+        }
+    }
+}
